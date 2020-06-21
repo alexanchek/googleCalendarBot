03b83872a916dac36144824730d3ea56c3aee45c
alexanchek <58663068+alexanchek@users.noreply.github.com>
2020-06-21 12:23:30 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,16 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-TOKEN = '1216192314:AAFkFLZcjICVIx0EiSkBo0oTexfJ5ppBHrI' # токен телеграм ботп
+TOKEN = '1216192314:AAFkFLZcjICVIx0EiSkBo0oTexfJ5ppBHrI' # токен телеграм бота
 
 # proxy config
 IP = 'ss-01.s5.ynvv.cc'
 PORT = '999'
 USER = '427107060'
 PASSWORD = 'SKWGuGsh'
+
+IP2 = 'ss-01.s5.ynvv.cc'
+PORT2 = '999'
+USER2 = '427107060'
+PASSWORD2 = 'SKWGuGsh'
+
