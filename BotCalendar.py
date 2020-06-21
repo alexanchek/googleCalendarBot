635cc058b088f8fb9527ae82b53e9526c9febb33
Alexanchek <dreamtheater@list.ru>
2020-06-21 12:19:36 +0300

    upcoming 10 events changed to events of the only this day.

--- BotCalendar.py
+++ BotCalendar.py
@@ -5,6 +5,8 @@ from config import *
 
 users = [427107060, 274293840]
 
+# TODO: CHANGE USER AGENT TO 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko)
+#  Chrome/56.0.2924.87 Safari/537.36' IN requests,utils.py
 apihelper.proxy = {
     'https': 'socks5://{}:{}@{}:{}'.format(USER, PASSWORD, IP, PORT)
 }
@@ -12,6 +14,7 @@ apihelper.proxy = {
 bot = telebot.TeleBot(TOKEN)
 
 
+# обработка команды event
 @bot.message_handler(func=lambda message: message.chat.id in users, commands=['event'])
 def get_text_messages(message):
     msg = upcomingEvents()
@@ -19,3 +22,5 @@ def get_text_messages(message):
 
 
 bot.polling(none_stop=True, interval=0, timeout=100)
+
+# TODO: добавить добавление и удаление событий в боте
--- calendarHandler.py
+++ calendarHandler.py
@@ -4,6 +4,8 @@ import pickle
 import datetime
 import dateutil.parser
 
+
+# работа с google calendar API
 def upcomingEvents():
     scopes = ['https://www.googleapis.com/auth/calendar']
 
@@ -14,17 +16,24 @@ def upcomingEvents():
 
     service = build("calendar", "v3", credentials=credentials)
 
-    now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
-    events_result = service.events().list(calendarId='primary', timeMin=now,
-                                          maxResults=10, singleEvents=True,
+    now = datetime.datetime.utcnow() - datetime.timedelta(hours=8.5) # вычитаем 8 часов, стартовая граница дня
+    now = now.isoformat() + 'Z'
+    endDay = datetime.datetime.now() + datetime.timedelta(hours=15.5) # прибавляем до финальной граниы дня
+    endDay = endDay.isoformat() + 'Z'
+
+    # показываем события на сегодняшний день
+    events_result = service.events().list(calendarId='primary', timeMin=now, timeMax=endDay,
+                                          singleEvents=True,
                                           orderBy='startTime').execute()
     events = events_result.get('items', [])
     msg = '<b>События на ближайшие дни:</b>\n'
     if not events:
-        msg = msg + 'тут пока ничего нет, хех'
+        msg = msg + 'На сегодня пока ничего, босс!'
     else:
         for event in events:
             start = event['start'].get('dateTime', event['start'].get('date'))
+
+            # кусок не работает почему-то
             # if not event['description']:
             #     print('нет описания')
             #     ev_desc = 'нет описания'
@@ -34,10 +43,11 @@ def upcomingEvents():
 
             ev_title = event['summary']
             cal_link = '<a href="%s">Подробнее...</a>' % event['htmlLink']
+            # берем дату
             ev_start = event['start'].get('dateTime')
+            # превращаем в удобоваримый формат день-месяц-год и время
             ev_start = dateutil.parser.parse(ev_start).strftime("%d.%m.%Y %H:%M")
             hr_line = '========================================================'
+            # собираем все воедино
             msg = msg + '%s\n%s\n%s\n%s\n\n' % (ev_title, ev_start, cal_link, hr_line)
     return msg
-
-
